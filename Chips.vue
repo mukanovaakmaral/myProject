9154ac7ac5f60d5a1dd96d14ced6fc55906ed865
mukanovaakmaral <126714295+mukanovaakmaral@users.noreply.github.com>
2023-03-07 10:09:43 +0600

    Update Chips.vue

--- Chips.vue
+++ Chips.vue
@@ -1,50 +1,112 @@
-<script setup lang="ts">
-import {ref} from 'vue'
-import Basic from "./Basic.vue";
-import Buttons from "./Buttons.vue";
-
-
-let defineConfig1 = defineConfig({
-  chips: [
-    {id: 1, selected: false},
-    {id: 2, selected: false},
-    {id: 3, selected: false},
-  ]
-})
-export default defineConfig1
-
-
-
-defineProps<{ value: string }>()
-
-const count = ref(0)
-
-
-
-</script>
-
 <template>
-  <h1>{{ value }}</h1>
+  <div class="wrapper"
+      @click="$emit('onClick',chip.id)"
+      :tabindex="tabindex"
 
-  <div>
-    <div class="content">
-      <Basic
-          v-for="chip in chips" :key="chip" :chip="chip" @onClick="setActiveChip" :suffix="chip.selected"
-      />
+  >
+    <div class="content"
+    :class="{active:active}">
+      <div class="icon1"
+           v-if="prefix">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24" >
+          <path
+              d="M237 771q54-38 115.5-56.5T480 696q66 0 127.5 18.5T723 771q35-41 52-91t17-104q0-129.675-91.23-220.838Q609.541 264 479.77 264 350 264 259 355.162 168 446.325 168 576q0 54 17 104t52 91Zm243-123q-60 0-102-42t-42-102q0-60 42-102t102-42q60 0 102 42t42 102q0 60-42 102t-102 42Zm.276 312Q401 960 331 930q-70-30-122.5-82.5T126 725.042q-30-69.959-30-149.5Q96 496 126 426.5t82.5-122Q261 252 330.958 222q69.959-30 149.5-30Q560 192 629.5 222t122 82.5Q804 357 834 426.724q30 69.725 30 149Q864 655 834 725q-30 70-82.5 122.5T629.276 930q-69.725 30-149 30ZM480 888q52 0 100-16.5t90-48.5q-43-27-91-41t-99-14q-51 0-99.5 13.5T290 823q42 32 90 48.5T480 888Zm0-312q30 0 51-21t21-51q0-30-21-51t-51-21q-30 0-51 21t-21 51q0 30 21 51t51 21Zm0-72Zm0 319Z"/>
+        </svg>
+      </div>
+      {{ text }}
+      <div class="icon2" v-if="suffix">
+        <svg xmlns="http://www.w3.org/2000/svg" height="15" viewBox="0 96 960 960" width="15" fill="#00a  153">
+          <path
+              d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/>
+        </svg>
+      </div>
     </div>
-    <Buttons/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "Chip",
+  data(){
+    return{
+      value:''
+    }
+  },
+  props: {
+    text: {
+      type: String,
+      required: true,
+      default: 'Submit'
+    },
+    chip: {
+      type: Object,
+      required: false
+    },
+    prefix: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    suffix: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    tabindex: {
+      type: Number,
+      required: false,
+      default: 1
+    },
+    active:{
+      type:Boolean,
+      required:false,
+      default:false
+    }
+  }
+}
+</script>
+
 <style scoped>
-h1 {
-  margin-bottom: 20px;
-}
-
 .content {
+  background-color: #F2F3F5;
+  border: 1px solid rgba(35, 54, 45, 0.12);
+  border-radius: 8px;
   display: flex;
-  gap: 4rem;
+  flex-direction: row;
+  height: 32px;
+  align-items: center;
+  width: fit-content;
+  padding: 0 12px;
+  cursor: pointer;
+  font-weight: 500;
+  font-size: 14px;
+  color: black;
+}
+.content:hover {
+  background-color: rgba(35, 54, 45, 0.08);
+}
+.wrapper:focus {
+  outline: none;
+  background: rgba(35, 54, 45, 0.12);
+  border: 1px solid #00A153!important;
+  border-radius: 8px;
+}
+.icon1 {
+  display: flex;
+  align-items: center;
+  margin-right: 8px;
+  fill: black;
+}
+.icon2 {
+  display: flex;
+  align-items: center;
+  margin-left: 8px;
+  fill: black;
 }
 
-
+.active {
+  background: rgba(35, 54, 45, 0.12)!important;
+  border: 1px solid #00A153!important;
+  border-radius: 8px;
+}
 </style>
